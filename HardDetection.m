b31da0ec20f5a8f1c51e4e3ba07c35103c8cf182
whatshow <wfwfpwefe2323@gmail.com>
2022-05-25 14:43:42 +1000

    remove redundant variables & simplify the interface

--- HardDetection.m
+++ HardDetection.m
@@ -1,26 +1,43 @@
 classdef HardDetection < handle
     properties
-        symbool_pool
+        symbol_pool     % a row vector of the constellation map
     end
     
     methods
         % constructor
-        % @symbool_pool: the constellation map
-        function self = HardDetection(symbool_pool)
+        % @symbool_pool: a vector of the constellation map
+        function self = HardDetection(symbol_pool)
             % type check
-            if ~isvector(symbool_pool)
+            if ~isvector(symbol_pool)
                 error("The constellation map (symbool_pool) must be a vector");
             end
             
-            % assign
-            if isrow(symbool_pool)
-                symbool_pool = symbool_pool.';
+            % transpose `symbol_pool` into a column vector & assign
+            if ~isrow(symbol_pool)
+                symbol_pool = symbol_pool.';
             end
-            self.symbool_pool = symbool_pool;
+            self.symbol_pool = symbol_pool;
         end
         
-        % hard detect
-        % detect
+        % hard estimate
+        % @syms:                    a vector the received symbols
+        % @origin_syms:             the original symbols (if give, ouput the ser)
+        % @origin_syms_ids:         the original symbol Ids (if give, ouput the ser)
+        function out = estim(self, syms)
+            % type check & transpose `syms` into a column vector
+            if ~isvector(syms)
+                error("The symbols to detect must be a vector.");
+            end
+            if ~iscolumn(syms)
+                syms = syms.';
+            end
+
+            % detect
+            syms_dist = abs(syms - self.symbol_pool).^2;                % calculate the minima distance
+            [~, min_syms_dist_inds] =  min(syms_dist,[],2);             % Obtain indices of minimum ecludian distance
+            out = self.symbol_pool(min_syms_dist_inds);                 % Obtain symbols of minimum ecludian distance
+        end
         
+        % hard detect 
     end
 end
